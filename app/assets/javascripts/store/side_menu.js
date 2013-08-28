1731d95766c36f3d330f177b35bf96747d757362
John Dyer <lbrapid@gmail.com>
2013-08-27 22:34:35 -0400

    Prepare install generator

---
+++ app/assets/javascripts/store/side_menu.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+  console.log($('#sidr-toggle'));
+  $('#sidr-toggle').sidr();
+});
--- app/assets/javascripts/store/spree_sidr.js
+++ app/assets/javascripts/store/spree_sidr.js
@@ -1,1 +1,3 @@
 //= require store/spree_frontend
+//= require jquery.sidr.min
+//= require_tree .
