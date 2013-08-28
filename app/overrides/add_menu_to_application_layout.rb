a9bf1fc68368ccf49ebd27adbabdfd6f61da8570
John Dyer <lbrapid@gmail.com>
2013-08-27 23:10:08 -0400

    Basic menu working

---
+++ app/overrides/add_menu_to_application_layout.rb
@@ -0,0 +1,24 @@
+Deface::Override.new(
+  virtual_path: 'spree/layouts/spree_application',
+  name: 'add_menu_to_application_layout',
+  insert_after: 'div.container',
+  text: '
+    <a id="sidr-toggle" href="#sidr">Toggle menu</a>
+    <div id="sidr">
+      <ul>
+        <li id="home-link"><%= link_to Spree.t(:home), spree.root_path %></li>
+        <li id="login-link"><%= link_to Spree.t(:login), spree.login_path %></li>
+      </ul>
+
+      <% allowed_controllers = %w{spree/home spree/products"} %>
+      <% if allowed_controllers.any? { |cont| cont == params[:controller] } %>
+        <ul>
+          <% get_taxonomies.each do |taxonomy| %>
+            <h6 class="taxonomy-root"><%= Spree.t(:shop_by_taxonomy, :taxonomy => taxonomy.name) %></h6>
+            <%= taxons_tree(taxonomy.root, @taxon, Spree::Config[:max_level_in_taxons_menu] || 1) %>
+          <% end %>
+        </ul>
+      <% end %>
+    </div>
+  '
+)
